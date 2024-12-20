386b716a6401f6d8e1104ce0e8fe6f996d2b7def
Ethan <757819951@qq.com>
2024-12-20 09:58:21 +0800

    实现SPU相关功能

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+from .views import (
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView,
+    SPUListView, SPUCreateView, SPUUpdateView, SPUDeleteView
+)
 
 app_name = 'gallery'
 
@@ -8,4 +11,8 @@ urlpatterns = [
     path('categories/add/', CategoryCreateView.as_view(), name='category_add'),
     path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category_edit'),
     path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+    path('spus/', SPUListView.as_view(), name='spu_list'),
+    path('spus/add/', SPUCreateView.as_view(), name='spu_add'),
+    path('spus/<int:pk>/edit/', SPUUpdateView.as_view(), name='spu_edit'),
+    path('spus/<int:pk>/delete/', SPUDeleteView.as_view(), name='spu_delete'),
 ] 
--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from .models import Category
+from .models import Category, SPU
 from django.contrib import messages
 
 # Create your views here.
@@ -81,4 +81,80 @@ class CategoryDeleteView(LoginRequiredMixin, DeleteView):
 
     def delete(self, request, *args, **kwargs):
         messages.success(request, '类目删除成功！')
+        return super().delete(request, *args, **kwargs)
+
+class SPUListView(LoginRequiredMixin, ListView):
+    model = SPU
+    template_name = 'gallery/spu_list.html'
+    context_object_name = 'spus'
+    paginate_by = 10
+    login_url = '/muggle/login/'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                spu_name__icontains=search_query
+            ) | queryset.filter(
+                spu_code__icontains=search_query
+            )
+        return queryset
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('search', '')
+        context['active_menu'] = 'spu'  # 用于高亮左侧菜单
+        return context
+
+class SPUCreateView(LoginRequiredMixin, CreateView):
+    model = SPU
+    template_name = 'gallery/spu_form.html'
+    fields = ['spu_code', 'spu_name', 'product_type', 'spu_remark', 
+             'sales_channel', 'category', 'status']
+    success_url = reverse_lazy('gallery:spu_list')
+    login_url = '/muggle/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '新增SPU'
+        context['active_menu'] = 'spu'
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, 'SPU创建成功！')
+        return super().form_valid(form)
+
+class SPUUpdateView(LoginRequiredMixin, UpdateView):
+    model = SPU
+    template_name = 'gallery/spu_form.html'
+    fields = ['spu_code', 'spu_name', 'product_type', 'spu_remark', 
+             'sales_channel', 'category', 'status']
+    success_url = reverse_lazy('gallery:spu_list')
+    login_url = '/muggle/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '编辑SPU'
+        context['active_menu'] = 'spu'
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, 'SPU更新成功！')
+        return super().form_valid(form)
+
+class SPUDeleteView(LoginRequiredMixin, DeleteView):
+    model = SPU
+    success_url = reverse_lazy('gallery:spu_list')
+    login_url = '/muggle/login/'
+    template_name = 'gallery/spu_confirm_delete.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '删除SPU'
+        context['active_menu'] = 'spu'
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'SPU删除成功！')
         return super().delete(request, *args, **kwargs)
